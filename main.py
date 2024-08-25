1fd72168148ffbf170c4a9bce1a48a26fb41122a
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-08-25 17:47:06 +0530

    Update deposit input prompt to include currency symbol

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def deposit():
     while True:
-        amount = input("Enter the amount you want to deposit: ")
+        amount = input("Enter the amount you want to deposit: $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
