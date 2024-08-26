ed1e26f8293dd97d9304936829c574e8fef9c3bf
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-08-26 16:10:10 +0530

    Include symbol count for each reel

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6
+}   
+
 def deposit():
     while True:
         amount = input("Enter the amount you want to deposit: $")
