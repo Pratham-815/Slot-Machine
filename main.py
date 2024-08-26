9d788aa22c632654cf3195f6c8590bc44f0a4302
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-08-26 16:27:59 +0530

    Add function to generate random symbols for each of the columns

--- main.py
+++ main.py
@@ -7,11 +7,33 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
-symbol_count = {
+symbols = {
     "A": 2,
     "B": 4,
-    "C": 6
+    "C": 6,
+    "D": 8
 }   
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 def deposit():
     while True:
